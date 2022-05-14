88d9573d27995c25f18ca4edc62782ea3ddd3841
3miliof <76463570+3miliof@users.noreply.github.com>
2022-05-14 22:55:57 +0100

    Verify Serial Number input and navigate to T&Cs

---
+++ app/src/main/java/com/example/to9deals/FeaturedPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.to9deals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeaturedPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_featured_page)
+    }
+}
